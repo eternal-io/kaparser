ebec70362fcb503c1ef8c5c034c183459d88ab5d
K--Alaptop <36335359+eternal-io@users.noreply.github.com>
2025-03-07 11:57:43 +0800

    Add `token_set!` macro.

--- src/macros.rs
+++ src/macros.rs
@@ -35,81 +35,104 @@ macro_rules! rep {
 
 //------------------------------------------------------------------------------
 
+/// Generate structures, implement [`Pattern`](crate::pattern::Pattern) for a single token conveniently.
 #[doc(hidden)]
 #[macro_export]
-macro_rules! token_set {
+macro_rules! token {
     () => {};
 }
 
-// /// Generate structures, implement [`Precede`] for a set of tokens conveniently.
-// #[macro_export]
-// macro_rules! token_set {
-//     ( $(
-//         $(#[$attr:meta])*
-//         $name:ident { $(
-//             $(#[$bttr:meta])*
-//             $key:ident = $word:literal
-//         ),* $(,)? }
-//     )* ) => { $( $crate::common::paste! {
-//       $(#[$attr])*
-//         #[doc = "\n\n*(generated token discriminant)*"]
-//         #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-//         pub(crate) enum [<$name Token>] { $(
-//           $(#[$bttr])*
-//             #[doc = "\n\nAssociates `` " $word " ``"]
-//             $key,
-//         )* }
-//
-//         impl [<$name Token>] {
-//             /// Returns the associated text.
-//             #[allow(dead_code, unreachable_patterns)]
-//             pub fn text(&self) -> &'static str {
-//                 match self {
-//                     $( Self::$key => token_set!( @validate $word ), )*
-//                     _ => unreachable!(),
-//                 }
-//             }
-//         }
-//
-//         #[doc = "Generated tokens pattern with [`" [<$name Token>] "`] discriminant."
-//                 "\n\nZST type by [`token_set!`] macro, only for passing as argument."]
-//         pub(crate) struct [<$name Tokens>];
-//
-//         impl Precede for [<$name Tokens>] {
-//             type Discriminant = [<$name Token>];
-//
-//             fn max_len(&self) -> usize {
-//                 const { token_set!( @max $($word.len(),)* 0 ) }
-//             }
-//
-//             fn matches(&self, _content: &str) -> Option<(usize, Self::Discriminant)> {
-//             $(
-//                 if _content.starts_with($word) {
-//                     return const { Some(($word.len(), Self::Discriminant::$key)) }
-//                 }
-//             )*
-//                 None
-//             }
-//         }
-//     } )* };
-//
-//     ( @max $expr:expr ) => { $expr };
-//
-//     ( @max $expr:expr, $( $exprs:expr ),+ ) => {{
-//         let a = $expr;
-//         let b = token_set!( @max $($exprs),+ );
-//
-//         if a > b { a } else { b }
-//     }};
-//
-//     ( @validate $word:literal ) => {
-//         const {
-//             let word = $word;
-//             assert!(
-//                 !word.is_empty() && word.len() <= 8192,
-//                 "the associated text must be non-empty string, and no more than 8192 bytes"
-//             );
-//             word
-//         }
-//     };
-// }
+//------------------------------------------------------------------------------
+
+/// Generate structures, implement [`Pattern`](crate::pattern::Pattern) for a set of tokens conveniently.
+#[doc(hidden)]
+#[macro_export]
+macro_rules! token_set {
+    ( $(
+        $(#[$attr:meta])*
+        $name:ident<$sli:ty> { $(
+            $(#[$bttr:meta])*
+            $key:ident = $word:literal
+        ),* $(,)? }
+    )* ) => { $( $crate::common::paste! {
+      $(#[$attr])*
+        #[doc = "\n\n*(generated token discriminant)*"]
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        pub(crate) enum $name { $(
+          $(#[$bttr])*
+            #[doc = "\n\nAssociates `` " $word " ``"]
+            $key,
+        )* }
+
+        #[doc = "Generated tokens pattern with [`" $name "`] discriminant."
+                "\n\nZST type by [`token_set!`] macro."]
+        pub(crate) struct [<$name T>];
+
+        impl<'i, E> Pattern<'i, $sli, E> for [<$name T>]
+        where
+            E: $crate::error::Situation,
+        {
+            type Captured = $name;
+            type Internal = Option<$name>;
+
+            #[inline(always)]
+            fn init(&self) -> Self::Internal {
+                None
+            }
+
+            #[inline(always)]
+            fn precede(&self, slice: &$sli, entry: &mut Self::Internal, eof: bool) -> Result<usize, E> {
+                let max_len = const { token_set!( @max $($word.len(),)* 0 ) };
+                if !eof && slice.len() < max_len {
+                    return E::raise_unfulfilled(Some((max_len - slice.len()).try_into().unwrap()));
+                }
+            $(
+                if slice.starts_with($word) {
+                    *entry = Some($name::$key);
+                    return const { Ok($word.len()) };
+                }
+            )*
+                E::raise_reject_at(0)
+            }
+
+            #[inline(always)]
+            fn extract(&self, _lice: &'i $sli, entry: Self::Internal) -> Self::Captured {
+                entry.unwrap()
+            }
+        }
+    } )* };
+
+    ( @max $expr:expr ) => { $expr };
+
+    ( @max $expr:expr, $( $exprs:expr ),+ ) => {{
+        let a = $expr;
+        let b = token_set!( @max $($exprs),+ );
+
+        if a > b { a } else { b }
+    }};
+}
+
+//------------------------------------------------------------------------------
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::*;
+
+    token_set! {
+        Boolean<str> {
+            False = "false",
+            True = "true",
+        }
+    }
+
+    #[test]
+    fn tk() {}
+
+    #[test]
+    fn tkst() {
+        let pat = __pat::<_, _, SimpleError>(BooleanT);
+        assert_eq!(pat.full_match("true").unwrap(), Boolean::True);
+        assert_eq!(pat.full_match("false").unwrap(), Boolean::False);
+        assert!(pat.full_match("False").is_err());
+    }
+}
