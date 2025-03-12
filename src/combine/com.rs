d531f085c6e39d9bbf002e9f20fb33a4a229cd51
K--Aethiax <36335359+eternal-io@users.noreply.github.com>
2025-03-12 12:44:50 +0800

    Tweak compound behavior.

--- src/combine/com.rs
+++ src/combine/com.rs
@@ -64,95 +64,85 @@ where
 }
 
 macro_rules! impl_compoundable_for_tuple {
-    ( $Alt:ident, $( $LabN:lifetime ~ $GenN:ident ~ $VarN:ident ~ $IdxN:tt )+ ) => { paste::paste! {
+    ( $( $GenN:ident ~ $IdxN:tt )+ ) => { paste::paste! {
         impl<'i, U, E, $($GenN: Pattern<'i, U, E>),+> Compoundable<'i, U, E> for ($($GenN,)+)
         where
             U: ?Sized + Slice + 'i,
             E: Situation,
         {
             type Captured = &'i U;
-            type Internal = (usize, $Alt<$($GenN::Internal),+>);
+            type Internal = usize;
 
             #[inline(always)]
             fn init_com(&self) -> Self::Internal {
-                (0, $Alt::Var1(self.0.init()))
+                0
             }
 
             #[inline(always)]
-            #[allow(irrefutable_let_patterns)]
             fn advance_com(&self, slice: &U, entry: &mut Self::Internal, eof: bool) -> Result<usize, E> {
-                use $Alt::*;
-                let (offset, states) = entry;
-
-                resume_advance! {
-                    states => { $(
-                        $LabN: $VarN(_) => [{
-                            *states = $VarN(self.$IdxN.init());
-                        }] {
-                            let $VarN(state) = states else { unreachable!() };
-                            match self.$IdxN.advance(slice.split_at(*offset).1, state, eof) {
-                                Ok(len) => *offset += len,
-                                Err(e) => return e.raise_backtrack(*offset),
-                            }
-                        }
-                    )+ }
+                *entry = 0;
+            $( {
+                let mut state = self.$IdxN.init();
+                match self.$IdxN.advance(slice.split_at(*entry).1, &mut state, eof) {
+                    Ok(len) => *entry += len,
+                    Err(e) => return e.raise_backtrack(*entry),
                 }
-
-                Ok(*offset)
+            } )+
+                Ok(*entry)
             }
 
             #[inline(always)]
             fn extract_com(&self, slice: &'i U, entry: Self::Internal) -> Self::Captured {
-                slice.split_at(entry.0).0
+                slice.split_at(entry).0
             }
         }
     } };
 }
 
 macro_rules! impl_compoundable_for_tuples {
-    (      $Lens1K:literal ~ $LabK:lifetime ~ $OrdK:literal ~ $IdxK:tt
-        $( $Lens1M:literal ~ $LabM:lifetime ~ $OrdM:literal ~ $IdxM:tt )*
+    (      $OrdK:literal ~ $IdxK:tt
+        $( $OrdM:literal ~ $IdxM:tt )*
     ) => {
         impl_compoundable_for_tuples! { @
-              $Lens1K ~ $LabK ~ $OrdK ~ $IdxK ;
-            $($Lens1M ~ $LabM ~ $OrdM ~ $IdxM)*
+              $OrdK ~ $IdxK ;
+            $($OrdM ~ $IdxM)*
         }
     };
 
-    ( @ $( $Lens1N:literal ~ $LabN:lifetime ~ $OrdN:literal ~ $IdxN:tt )+ ;
-           $Lens1K:literal ~ $LabK:lifetime ~ $OrdK:literal ~ $IdxK:tt
-        $( $Lens1M:literal ~ $LabM:lifetime ~ $OrdM:literal ~ $IdxM:tt )*
+    ( @ $( $OrdN:literal ~ $IdxN:tt )+ ;
+           $OrdK:literal ~ $IdxK:tt
+        $( $OrdM:literal ~ $IdxM:tt )*
     ) => { paste::paste! {
-        impl_compoundable_for_tuple!( [<Alt $Lens1K>], $($LabN ~ [<P $OrdN>] ~ [<Var $OrdN>] ~ $IdxN)+ );
+        impl_compoundable_for_tuple!( $([<P $OrdN>] ~ $IdxN)+ );
 
         impl_compoundable_for_tuples! { @
-            $($Lens1N ~ $LabN ~ $OrdN ~ $IdxN)+
-              $Lens1K ~ $LabK ~ $OrdK ~ $IdxK ;
-            $($Lens1M ~ $LabM ~ $OrdM ~ $IdxM)*
+            $($OrdN ~ $IdxN)+
+              $OrdK ~ $IdxK ;
+            $($OrdM ~ $IdxM)*
         }
     } };
 
-    ( @ $( $Lens1N:literal ~ $LabN:lifetime ~ $OrdN:literal ~ $IdxN:tt )+ ; ) => {};
+    ( @ $( $OrdN:literal ~ $IdxN:tt )+ ; ) => {};
 }
 
 impl_compoundable_for_tuples! {
-    0  ~ 'p1  ~ 1  ~ 0
-    1  ~ 'p2  ~ 2  ~ 1
-    2  ~ 'p3  ~ 3  ~ 2
-    3  ~ 'p4  ~ 4  ~ 3
-    4  ~ 'p5  ~ 5  ~ 4
-    5  ~ 'p6  ~ 6  ~ 5
-    6  ~ 'p7  ~ 7  ~ 6
-    7  ~ 'p8  ~ 8  ~ 7
-    8  ~ 'p9  ~ 9  ~ 8
-    9  ~ 'p10 ~ 10 ~ 9
-    10 ~ 'p11 ~ 11 ~ 10
-    11 ~ 'p12 ~ 12 ~ 11
-    12 ~ 'p13 ~ 13 ~ 12
-    13 ~ 'p14 ~ 14 ~ 13
-    14 ~ 'p15 ~ 15 ~ 14
-    15 ~ 'p16 ~ 16 ~ 15
-    16 ~ 'p17 ~ 17 ~ 16
+    1  ~ 0
+    2  ~ 1
+    3  ~ 2
+    4  ~ 3
+    5  ~ 4
+    6  ~ 5
+    7  ~ 6
+    8  ~ 7
+    9  ~ 8
+    10 ~ 9
+    11 ~ 10
+    12 ~ 11
+    13 ~ 12
+    14 ~ 13
+    15 ~ 14
+    16 ~ 15
+    17 ~ 16
 }
 
 //------------------------------------------------------------------------------
