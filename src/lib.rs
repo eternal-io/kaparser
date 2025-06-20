786a49ffab189d6020b2d00099976d24fbc9dafc
K--Aethiax <36335359+eternal-io@users.noreply.github.com>
2025-06-20 16:22:38 +0800

    archive old codes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,6 @@
 #![no_std]
 #![deny(unsafe_code)]
 
-#[macro_use]
-mod macros;
-
-#[macro_use]
-pub mod common;
-pub mod combine;
-pub mod error;
-pub mod line_col;
-pub mod pattern;
-pub mod predicate;
-pub mod prelude;
-pub mod stream;
-
-#[cfg(test)]
-mod tester;
-
 #[cfg(test)]
 #[cfg_attr(test, macro_use)]
 extern crate std;
